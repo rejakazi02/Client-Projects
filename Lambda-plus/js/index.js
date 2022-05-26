16f9478240356b8213e9ab634307605b09addaa3
Reja Kazi <rejakazi02@gmail.com>
2022-05-26 21:27:01 +0600

    dfrtg

--- Lambda-plus/js/index.js
+++ Lambda-plus/js/index.js
@@ -87,3 +87,67 @@ slideSpans[0].onclick = () => {
 
 
 // cart icon work 
+
+
+let carts = document.querySelectorAll('.add-cart');
+let po = document.getElementById('')
+let products = [
+    {
+        name: 'Grey Tshirt',
+        tag: 'greytshirt',
+        price: 15,
+        inCart: 0
+    },
+    
+    {
+        name: 'Grey huddie',
+        tag: 'greyhuddie',
+        price: 25,
+        inCart: 0
+    },
+    {
+        name: 'Black Tshirt',
+        tag: 'blacktshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Black Huddie',
+        tag: 'blackhuddie',
+        price: 55,
+        inCart: 0
+    }
+
+];
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers();
+    })
+    
+}
+
+
+function  cartNumbers() {
+    
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+    if (productNumbers) {
+
+        
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.test').textContent = productNumbers + 1;
+    }
+
+
+    else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.test').textContent = 1;
+    }
+
+
+
+
+    
+}
