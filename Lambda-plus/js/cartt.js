abcab43971d7a8bb2f45ce36113608433fb08f53
Reja Kazi <rejakazi02@gmail.com>
2022-05-28 19:13:58 +0600

    cart number add

---
+++ Lambda-plus/js/cartt.js
@@ -0,0 +1,81 @@
+
+let carts = document.querySelectorAll('.add-cart');
+let products = [
+
+    {
+        name: 'sari',
+        price: 280,
+        inCart: 0
+    },
+    {
+        name: 'sari',
+        price: 28,
+        inCart: 0
+    },
+    {
+        name: 'sari',
+        price: 280,
+        inCart: 0
+    },
+    {
+        name: 'sari',
+        price: 28,
+        inCart: 0
+    }
+];
+
+
+for(let i=0; i < carts.length; i++){
+    carts[i].addEventListener('click', () => {
+
+        
+    })
+}
+
+function onCartNumbers() {
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if( productNumbers ) {
+       
+      let pp =  document.querySelector('.Cart').textContent = productNumbers;
+   
+    // return pp;
+    }
+}
+
+function cartNumbers() {
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+
+    productNumbers = parseInt(productNumbers);
+
+    if( productNumbers ) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+      let p =  document.querySelector('.Cart').textContent = productNumbers + 1;
+   
+    return p;
+    }
+
+    else {
+         localStorage.setItem('cartNumbers', 1);
+            document.querySelector('.Cart').textContent = 1;
+
+ 
+        }
+
+  
+
+}
+
+
+
+    let p = document.getElementById('carttt');
+
+    p.textContent = cartNumbers();
+    
+
+
+
+    onCartNumbers();
